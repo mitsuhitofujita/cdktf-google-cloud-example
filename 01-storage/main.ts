079218e5cb9697394a3d23c1f7f64dfeda781f24
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-04-01 15:05:33 +0000

    Add storage settings

--- 01-storage/main.ts
+++ 01-storage/main.ts
@@ -1,11 +1,23 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import * as google from '@cdktf/provider-google';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new google.provider.GoogleProvider(this, 'google', {
+      project: 'cdktf-example',
+    });
+
+    new google.storageBucket.StorageBucket(this, 'storage', {
+      location: 'asia-northeast1',
+      name: 'cdk-example-terraform-backend',
+      storageClass: "STANDARD",
+      versioning: {
+        enabled: true,
+      },
+    });
   }
 }
 
